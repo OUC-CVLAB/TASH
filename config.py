b0a59e8639c574653ba9f4e283bf9a121679f9ae
OUC-CVLAB <buguanren@stu.ouc.edu.cn>
2025-09-04 17:13:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,47 @@
+import argparse
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import numpy as np
+import warnings
+import os
+import timm
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image
+import kornia.augmentation as Kg
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+from collections import OrderedDict
+
+from easydict import EasyDict as edict
+
+
+config = edict()
+
+config.patches = edict()
+config.patches.size = (16, 16)
+
+config.split = 'non-overlap'
+config.slide_step = 12
+config.hidden_size = 768
+
+config.transformer = edict()
+config.transformer.mlp_dim = 3072
+config.transformer.num_heads = 12
+config.transformer.num_layers = 12
+config.transformer.attention_dropout_rate = 0.0
+config.transformer.dropout_rate = 0.1
+
+config.classifier = 'token'
+config.representation_size = None
+
+
+
+# copy args to config（used in main_DHD.py）
+def update_from_args(args):
+    config.update(vars(args))
+
+# from config import config, update_from_args
+# update_from_args(args)
+# print(config.batch_size)
